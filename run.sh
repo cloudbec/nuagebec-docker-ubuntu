31f8c90d0a237e848f579be654e57c4569210393
Michael Faille <michael@faille.io>
2015-02-23 13:01:12 -0500

    Force upcased check for env. var. configs.
    ie : UPCASED_UPCASED_SSH_SERVER = `echo "$SSH_SERVER" | tr '[a-z]' '[A-Z]'`

--- run.sh
+++ run.sh
@@ -3,8 +3,11 @@ if [ ! -f /data/persistent/.root_pw_set ]; then
 	/data/set_root_pw.sh
 fi
 
+
+
 # see this bug report for more info https://github.com/cloudbec/nuagebec-docker-ubuntu/issues/1
-if [  "$SSH_SERVER" == "false"  ] || [  "$SSH_SERVER" == "FALSE"  ] || [  "$SSH_SERVER" == "0"  ]; then
+UPCASED_UPCASED_SSH_SERVER = `echo "$SSH_SERVER" | tr '[a-z]' '[A-Z]'`
+if [  "$UPCASED_SSH_SERVER" == "FALSE"  ] || [  "$UPCASED_SSH_SERVER" == "0"  ]; then
     sed -i 's/autostart=true/autostart=false/g' /etc/supervisor/conf.d/sshd.conf
 fi
 
