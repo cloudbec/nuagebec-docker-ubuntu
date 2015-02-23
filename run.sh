2f40a85aa3cdf163411cb81e4435b2fb1036e425
Michael Faille <michael@faille.io>
2015-02-23 12:49:13 -0500

    ADD feature to disable SSH

--- run.sh
+++ run.sh
@@ -3,4 +3,11 @@ if [ ! -f /data/persistent/.root_pw_set ]; then
 	/data/set_root_pw.sh
 fi
 
+
+
+if [  "$SSH_SERVER" == "false"  ] || [  "$SSH_SERVER" == "FALSE"  ] || [  "$SSH_SERVER" == "0"  ]; then
+    sed -i 's/autostart=true/autostart=false/g' /etc/supervisor/conf.d/sshd.conf
+fi
+
+
 exec supervisord -n -c /etc/supervisor/supervisord.conf
