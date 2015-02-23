d39866a1bb5f7155982084a81f07ffc2eff866dd
Michael Faille <michael@faille.io>
2015-02-23 12:52:39 -0500

    Some minimalist documentation and I clear exceeding whitespaces.

--- run.sh
+++ run.sh
@@ -3,11 +3,9 @@ if [ ! -f /data/persistent/.root_pw_set ]; then
 	/data/set_root_pw.sh
 fi
 
-
-
+# see this bug report for more info https://github.com/cloudbec/nuagebec-docker-ubuntu/issues/1
 if [  "$SSH_SERVER" == "false"  ] || [  "$SSH_SERVER" == "FALSE"  ] || [  "$SSH_SERVER" == "0"  ]; then
     sed -i 's/autostart=true/autostart=false/g' /etc/supervisor/conf.d/sshd.conf
 fi
-
 
 exec supervisord -n -c /etc/supervisor/supervisord.conf
