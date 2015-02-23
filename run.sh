2a64e31db1b6463f471edf4decdb96dd40f1f1b0
Michael Faille <michael@faille.io>
2015-02-23 13:02:46 -0500

    CLEAN whitespaces

--- run.sh
+++ run.sh
@@ -3,8 +3,6 @@ if [ ! -f /data/persistent/.root_pw_set ]; then
 	/data/set_root_pw.sh
 fi
 
-
-
 # see this bug report for more info https://github.com/cloudbec/nuagebec-docker-ubuntu/issues/1
 UPCASED_UPCASED_SSH_SERVER = `echo "$SSH_SERVER" | tr '[a-z]' '[A-Z]'`
 if [  "$UPCASED_SSH_SERVER" == "FALSE"  ] || [  "$UPCASED_SSH_SERVER" == "0"  ]; then
