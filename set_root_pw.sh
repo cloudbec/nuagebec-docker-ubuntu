a950c394db55dbe57151c3244ec4f54352acadc0
Michael Faille <michael@faille.io>
2015-02-26 12:43:15 -0500

    Change docker startup message to reflect ssh configs

--- set_root_pw.sh
+++ set_root_pw.sh
@@ -13,13 +13,30 @@ echo "root:$PASS" | chpasswd
 echo "=> Done!"
 touch /data/persistent/.root_pw_set
 
-echo "========================================================================"
-echo "You can now connect to this Ubuntu container via SSH using:"
-echo ""
-echo " for reach 1st interface ip"
-echo " ssh -p <port> root@"$(ip -o -4 addr show | awk -F '[ /]+' '/global/ {print $4}' | head -n1)
-echo ""
-echo "and enter the root password '$PASS' when prompted"
-echo ""
-echo "Please remember to change the above password as soon as possible!"
-echo "========================================================================"
+UPCASED_UPCASED_SSH_SERVER=`echo "$SSH_SERVER" | tr '[a-z]' '[A-Z]'`
+if [  "$UPCASED_SSH_SERVER" == "FALSE"  ] || [  "$UPCASED_SSH_SERVER" == "0"  ]; then
+
+    echo "========================================================================"
+    echo You can now connect to this Ubuntu container
+    echo ""
+    echo To acces docker TTY locally
+    echo " docker exec -it [container-id] bash"
+    echo ""
+    echo The root password is : '$PASS'
+    echo ""
+    echo "Please remember to change the above password as soon as possible!"
+
+else
+
+    echo "========================================================================"
+    echo "You can now connect to this Ubuntu container"
+    echo ""
+    echo " Using SSH, for reach 1st interface ip at port 22"
+    echo " ssh root@"$(ip -o -4 addr show | awk -F '[ /]+' '/global/ {print $4}' | head -n1)
+    echo ""
+    echo "and enter the root password '$PASS' when prompted"
+    echo ""
+    echo "Please remember to change the above password as soon as possible!"
+    echo "========================================================================"
+
+fi
