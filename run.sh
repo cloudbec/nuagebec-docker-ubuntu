5af53fe957570970e47779d73e9c35eb01ebaf1c
Michael Faille <michael@faille.io>
2015-02-26 11:46:10 -0500

    FIX remove space near "=" because bash script dont accept it for variable assigment..

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ if [ ! -f /data/persistent/.root_pw_set ]; then
 fi
 
 # see this bug report for more info https://github.com/cloudbec/nuagebec-docker-ubuntu/issues/1
-UPCASED_UPCASED_SSH_SERVER = `echo "$SSH_SERVER" | tr '[a-z]' '[A-Z]'`
+UPCASED_UPCASED_SSH_SERVER=`echo "$SSH_SERVER" | tr '[a-z]' '[A-Z]'`
 if [  "$UPCASED_SSH_SERVER" == "FALSE"  ] || [  "$UPCASED_SSH_SERVER" == "0"  ]; then
     sed -i 's/autostart=true/autostart=false/g' /etc/supervisor/conf.d/sshd.conf
 fi
